c5652b0be940475e845ab8aba316211c09c6c848
Antofe97 <antonio.felipe97@gmail.com>
2021-03-18 17:13:37 +0100

    Create Notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "notes#index"
   
-  get "/notes", to: "notes#index"
+  resources :notes
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
