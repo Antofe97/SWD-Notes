0d80f0acfcf9a0afc52e8eade41ce19dcb10478b
IsabelDM <Isabel.Diezma1@alu.uclm.es>
2021-04-20 00:09:58 +0200

    Big Commit Devise and Styles
    Devise Gem, creation of the navbar and change of background and some styles.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  
   root "notes#index"
   resources :notes #plural 
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
