de6b95bc2baa65e18ac3d900950c985287c88330
IsabelDM <Isabel.Diezma1@alu.uclm.es>
2021-05-04 23:46:47 +0200

    Final Boostrap + Controllers Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
-
+    validates_uniqueness_of :email
     has_many :notes, dependent: :destroy
     has_many :collections, dependent: :destroy
 end
